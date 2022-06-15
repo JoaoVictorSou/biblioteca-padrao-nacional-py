ec3f25129da7727b708b3e3ec0e4d5d8407b0156
JoaoVictorSou <soulima.joao@gmail.com>
2022-06-15 04:39:59 -0300

    exibição do CPF utilizando boas práticas do Python

--- Cpf.py
+++ Cpf.py
@@ -1,7 +1,10 @@
 class Cpf:
     def __init__(self, numero_cpf: str):
-        self.numero_documento = numero_cpf if Cpf.cpf_valido(numero_cpf) else ""
+        self.__numero_documento = numero_cpf if Cpf.cpf_valido(numero_cpf) else ""
     
+    def __str__(self):
+        return Cpf.monta_estrutura(self.__numero_documento)
+
     @staticmethod
     def cpf_valido(numero_cpf: str):
         # Verificação se há apenas números no valor informado
@@ -16,7 +19,8 @@ class Cpf:
             raise ValueError("CPF deve conter exatamente 11 dígitos.")
         
     @staticmethod
-    def monta_estrutura(numero_cpf):
+    def monta_estrutura(numero_cpf: str):
         if Cpf.cpf_valido(numero_cpf):
             str_numero_cpf = str(numero_cpf)
+            # Cortando o CPF com base em 000.000.000-00
             return f"{str_numero_cpf[:3]}.{str_numero_cpf[3:6]}.{str_numero_cpf[6:9]}-{str_numero_cpf[9:]}"

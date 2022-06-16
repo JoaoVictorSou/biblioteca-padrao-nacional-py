d049f3a07b1c6cedbda765542f3531cc145da7e0
JoaoVictorSou <soulima.joao@gmail.com>
2022-06-16 01:21:37 -0300

    validador de CNPJ

---
+++ Cnpj.py
@@ -0,0 +1,24 @@
+from validate_docbr import CNPJ
+
+class Cnpj:
+    def __init__(self, numero_cnpj: str):
+        if Cnpj.cnpj_valido(numero_cnpj):
+            self.__numero_documento = numero_cnpj
+        else:
+            raise ValueError("CPNJ inválido!")
+
+    def __str__(self):
+        return self.__numero_documento
+
+    @staticmethod
+    def cnpj_valido(numero_cnpj):
+        cnpj = CNPJ()
+        # VALIDAÇÃO SE POSSUE APENAS ALGARISMOS NUMÉRICOS
+        int(numero_cnpj)
+
+        # VALIDAÇÃO QUANTIDADE DE ALGARISMOS
+        str_numero_cnpj = str(numero_cnpj)
+        if len(str_numero_cnpj) == 14:
+            return cnpj.validate(numero_cnpj)
+        else:
+            raise ValueError("CNPJ deve conter 14 dígitos!")
